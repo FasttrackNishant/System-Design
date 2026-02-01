8ac3ab96cdd56c8110482923bc9a162f8aa40ba3
Nishant Patil <ncp1435@gmail.com>
2026-02-01 16:08:47 +0530

    LRU Cache Java

---
+++ LLDAllCodes/src/easy/lrucache/Node.java
@@ -0,0 +1,12 @@
+package easy.lrucache;
+
+public class Node<K, V> {
+    K key;
+    V value;
+    Node<K, V> prev, next;
+
+    Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
